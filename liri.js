c4d2d729817878eeafff2ec6b8e5b963fb88fbee
Planet15 <parmar0815@gmail.com>
2019-03-26 20:14:16 -0400

    created switch commands and started on the functions

--- liri.js
+++ liri.js
@@ -1,3 +1,71 @@
 require("dotenv").config();
 var keys = require("./keys");
+
+// axios
+var axios = require("axios");
+
+// keys
 var spotify = new Spotify(keys.spotify);
+
+// time
+var moment = require("moment");
+
+// arguments
+var go = process.argv[2];
+var value = process.argv.slice(3).join("+");
+
+// switch commands
+switch(go) {
+    case "concert-this":
+    concert();
+    break;
+
+    case "spotify-this-song":
+    spotify();
+    break;
+
+    case "movie-this":
+    movie();
+    break;
+
+    case "do-what-it-says":
+    whatItSays();
+    break;
+}
+
+// creating the fs variable, and adding if else statements to it 
+// so that we can get some sort of output weather its an error or the acutal data
+var fs = require("fs");
+
+fs.readFile("random.txt", "utf-8", function(err, data) {
+    if(err) {
+
+        console.log(err);
+    }
+
+    console.log(data);
+})
+
+axios.get("")
+
+// declaring the concert function that we called in the switch commands
+function concert() {
+    var artist = value;
+
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+    .then(
+        function(concertResponse) {
+            for (var i = 0; i < concertResponse.data.length; i++) {
+
+                var concert = concertResponse.data[i].venue
+                console.log("Concert Date: " + moment(concert.date).format("L"));
+                console.log("Venue Name: " + concert.name);
+                console.log("Venue City: " + concert.city); 
+            }
+        }
+    )
+}
+
+
+// declaring movie/OMDB function that was called in the switch commands
+function 
